cf4a6d7a5e06b0763fb2f6431af72ad0474aa8e9
OlegShitov <olegshitov5@gmail.com>
2022-10-13 12:51:58 +0300

    First react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import TodoList from './TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const date1 = new Date(2021, 7, 19, 14, 5)
+const date2 = new Date(2021, 7, 19, 15, 23)
+
+const initialData = [
+  {
+    title: 'Изучить Реакт',
+    desc: 'Да по скорее!',
+    image: '',
+    done: true,
+    createdAt: date1.toLocaleString,
+    key: date1.getTime()
+  },
+  {
+    title: 'Написать первое Реакт приложение',
+    desc: 'Список запланированных дел',
+    image: '',
+    done: false,
+    createdAt: date2.toLocaleString,
+    key: date2.getTime()
+  }
+]
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.data = initialData
+    this.setDone = this.setDone.bind(this)
+  }
+  setDone(key) {
+    const deed = this.data.find((current) => {
+      current.key === key
+    })
+    deed.done = true
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar is-light">
+          <div className="navbar-brand">
+            <span className="navbar-item is-uppercase">
+              Todos
+            </span>
+          </div>
+        </nav>
+        <main className="content px-6 mt-6">
+          <TodoList list={this.data} setDone={this.setDone} />
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
